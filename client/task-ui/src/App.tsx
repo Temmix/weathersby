eaaf81a671125f0560af6aa2f4e37619a3c4dc93
Temi Makinde <Temi.Makinde@Gmail.com>
2024-02-02 17:12:45 +0000

    removed some resources

--- client/task-ui/src/App.tsx
+++ client/task-ui/src/App.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import "./App.css";
 import { List } from "./components/task/list";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Item } from "./components/task/item";
-//import { Route } from "react-router";
-import { View } from "./components/task/view";
 import { CreateTask } from "./components/task/create";
 
 function App() {
@@ -13,8 +10,6 @@ function App() {
       <Routes>
         <Route path='/tasks' element={<List />} />
         <Route path='/tasks/create' element={<CreateTask />} />
-        <Route path='/tasks/:id' element={<Item />} />
-        <Route path='/tasks/:id' element={<View />} />
       </Routes>
     </Router>
   );
